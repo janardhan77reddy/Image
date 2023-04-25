675b33fb4037acdf29b229e3ebcca4d9c60ed52f
janardhan77reddy <reddyjanardhan839@gmail.com>
2023-04-25 22:30:17 +0530

    Chnages

---
+++ src/imagegrid.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+
+
+const ImageGrid = ({ images }) => {
+
+   
+    return (
+        <div className="image-grid">
+            {images.map((image) => (
+                <img key={image.id} src={image.urls.regular} alt={image.alt_description} width="350px" height="350px"/>
+            ))}
+        </div>
+    );
+};
+
+
+
+export default ImageGrid;
